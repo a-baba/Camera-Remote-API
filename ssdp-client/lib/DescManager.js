a9f418e3eee00805e2b709a4120b41137463c02b
kensakukomatsu <kensaku.komatsu@gmail.com>
2014-10-08 21:18:46 +0900

    DescManger

--- ssdp-client/lib/DescManager.js
+++ ssdp-client/lib/DescManager.js
@@ -1,10 +1,39 @@
-var http = new 
-
+var http = require('http');
 
 var DescManager = {};
 
 
-DescManager.get = function(url){
+
+DescManager.get = function(url, nodes, callback){
+  if(!nodes) throw "nodes should be specified"
+  http.get(url, function(res, e) {
+    if(!e) {
+      var body = "";
+      res.setEncoding('utf8');
+      res.on('data', function(chunk) {
+        body += chunk;
+      });
+
+      res.on('end', function(){
+        var res = {};
+        nodes.forEach(function(node) {
+          var re = new RegExp("<" + node + ">(.+?)</" + node + ">");
+          var f = body.match(re);
+          res[node] = f ? f[1]: null;
+        });
+
+
+        if(typeof(callback) !== "function") {
+          console.log(res);
+          console.log(body);
+        } else {
+          callback(res);
+        }
+      });
+    } else {
+      throw e.message;
+    }
+  });
 }
 
 
@@ -13,5 +42,11 @@ module.exports = DescManager;
 // test code
 //
 if(!__filename.match('index.js')) {
-  
+  DescManager.get(
+    'http://192.168.40.10:64321/scalarwebapi_dd.xml', 
+    [
+      "av:X_ScalarWebAPI_ActionList_URL",
+      "av:X_ScalarWebAPI_LiveView_URL"
+    ]
+  );
 }
