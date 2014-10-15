8fa543e299a5d9c3fb964fd6fab9ba6c5872aa0e
kensakukomatsu <kensaku.komatsu@gmail.com>
2014-10-15 19:08:32 +0900

    code reviewed

--- ssdp-client/index.js
+++ ssdp-client/index.js
@@ -7,7 +7,6 @@ var SONY_CameraAPI = require('./lib/SONY_CameraAPI')
 
 WoTController.init();
 
-var MAX_SIZE = 100000;
 var COMMON_HEADER_SIZE = 8;
 var PAYLOAD_HEADER_SIZE = 128;
 
@@ -63,6 +62,7 @@ wss.on('connection', function(ws) {
 
     switch(m.method){
     case "liveview":
+      // 今は決め打ち
       var urn = "urn:schemas-sony-com:service:ScalarWebAPI:1";
 
       WoTController.setDevice(urn, "uuid:00000000-0005-0010-8000-fcc2de538943::urn:schemas-sony-com:service:ScalarWebAPI:1"); 
@@ -71,12 +71,17 @@ wss.on('connection', function(ws) {
       console.log(device);
       
       var plug = new SONY_CameraAPI(device);
+
+      // ここまで決め打ち部分
+
+
+      // sony camの初期化
       plug.init(function(){
 
-        var img_ab;
-        var img_dv;
-        var img_off = 0;
-        var img_data_size = 0;
+        var img_ab;  // imageを格納する array buffer
+        var img_dv;  // 上記array bufferに対するdata view
+        var img_off = 0; // 画像のoffset値
+        var img_data_size = 0; // 画像のデータサイズ（payloadヘッダから都度取得）
 
         plug.getLiveView(function(data) {
 
@@ -84,6 +89,8 @@ wss.on('connection', function(ws) {
           var payload_header_flag = false;
           var ab = new ArrayBuffer(data.length);
           var dv = new DataView(ab);
+
+          // chunkデータを一旦、arraybuffer(ab)に入れる
           for(var i=0; i<data.length; i++){
             dv.setUint8(i, data[i]);
           }
@@ -99,6 +106,7 @@ wss.on('connection', function(ws) {
             var sequence_number = dv.getUint16(2);
             var time_stamp = dv.getUint32(4);
 
+            // fixme (start_byte === 255 && payload_type === 1)
             if(start_byte.toString(16) == "ff" &&  payload_type.toString(16) == "1"){
               console.log("This is Common Header! ----------------------------");
 
@@ -126,9 +134,9 @@ wss.on('connection', function(ws) {
             var padding_size = dv.getUint8(15);
             var reserved1 = dv.getUint32(16);
             var flag = dv.getUint8(20);
-            var reserved2 = dv.getUint32(16);
 
-            reserved2 = [];
+            var reserved2 = [];
+
             for (var i=0, off=21; i<115; i++) {
               reserved2[i] = dv.getUint8(i + off);
             }
@@ -160,12 +168,11 @@ wss.on('connection', function(ws) {
           }
 
           //Payload Headerがあったら、オフセットを0にする
-          if(payload_header_flag) img_off = 0;
+          if(common_header_flag && payload_header_flag) img_off = 0;
 
           //パケットにヘッダが含まれていたら、JPEGデータの開始位置をヘッダの直後に合わせる
           var begin = 0;
-          if(common_header_flag) begin += COMMON_HEADER_SIZE;
-          if(payload_header_flag) begin += PAYLOAD_HEADER_SIZE;
+          if(common_header_flag && payload_header_flag) begin += (COMMON_HEADER_SIZE + PAYLOAD_HEADER_SIZE);
 
           // JPEGデータをDataViewに入れる
           for (var i=begin; i<data.length; i++) {
@@ -173,7 +180,7 @@ wss.on('connection', function(ws) {
           }
 
           //オフセットを調整
-          img_off += data.length - begin;
+          img_off += ( data.length - begin );
           var datasize = data.length - begin;
           console.log("datasize: " + datasize);
           console.log("offset: " + img_off);
